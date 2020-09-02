4515bf11a2873c8ebe5cade0aec042559155fb4e
keyiflerolsun <keyiflerolsun@gmail.com>
2020-09-02 21:12:59 +0300

    sistem eklentisi güncellendi

--- kekikRobot/roBot/botAlani/Eklentiler/sistem.py
+++ kekikRobot/roBot/botAlani/Eklentiler/sistem.py
@@ -6,10 +6,10 @@ import requests, os, platform, psutil
 from konsolTaban import oturum, cihaz, ip
 from datetime import datetime
 
-@Client.on_message(filters.command(['sistem'], ['!','.','/']) & filters.me)
+@Client.on_message(filters.command(['sistem'], ['!','.','/']))
 async def sistem(client, message):
     # < Başlangıç
-    uyku = await message.edit("__asyncio.sleep(0.3)__")
+    uyku = await message.reply("__asyncio.sleep(0.3)__")
     await asyncio.sleep(0.3)
     
     cevaplanan_mesaj    = message.reply_to_message
