182631b3a184ec1a61664f7d0861ed9bbc3396a4
keyiflerolsun <keyiflerolsun@gmail.com>
2020-09-02 20:32:09 +0300

    bildirim modüllerine try except blokları eklendi

--- kekikRobot/konsolTaban/__init__.py
+++ kekikRobot/konsolTaban/__init__.py
@@ -84,13 +84,23 @@ def bildirim():
     elif platform.machine() == "aarch64":
         pass 
     elif isletim_sistemi == "Windows" and bellenim_surumu >= "10":
-        from win10toast import ToastNotifier
+        
+        try:
+            from win10toast import ToastNotifier
+        except:
+            os.system('pip install win10toast')
+        
         bildirim = ToastNotifier()
         bildirim.show_toast(f"{pencere_basligi}", f"{bildirim_metni}",
             icon_path=None, duration=10, threaded=True
             )
     elif isletim_sistemi == "Linux":
-        import notify2
+        
+        try:
+            import notify2
+        except:
+            os.system('pip install notify2')
+        
         notify2.init(pencere_basligi)
         bildirim = notify2.Notification(f"{pencere_basligi}", f"{bildirim_metni}", "notification-message-im")
         bildirim.show()
